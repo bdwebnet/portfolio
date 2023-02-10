d1a24af68ab34775a1da5acc9fb6487af897071d
bd <51322242+bdwebnet@users.noreply.github.com>
2023-02-11 00:57:33 +0100

    updated project texts

--- utils/projects.js
+++ utils/projects.js
@@ -1,43 +1,43 @@
 const projects = [
   {
-    title: 'Tim-Reiter.de',
-    link: 'https://tim-reiter.de',
-    description: 'WordPress-Website für einen Fahrer in der ADAC GT4 Germany.'
-  },
-  {
-    title: 'Geocaching-Adventskalender',
-    link: 'https://advent.dalgeo.de',
-    description: 'Rätsel-Adventskalender für Geocaching.'
-  },
-  {
-    title: 'Praxis Dr. Medwed',
-    link: 'https://praxis-medwed.de',
-    description: 'Praxiswebsite für die Praxis Dr. Medwed'
-  },
-  {
-    title: 'ADFC Ostfildern',
-    link: 'https://lastenrad-ostfildern.de',
-    description: 'Informationssite und Buchungssystem für das Lastenrad des ADFCs Ostfildern.'
-  },
-  {
     title: 'QRCheck',
     link: 'https://qrcheck.dev',
-    description: 'Backend für einen QR-Scanner mit Virenschutz.'
+    description: 'QRCheck ist eine App für Android und iOS, mit der QR-Codes gescannt, erstellt und URLs auf bösartige Websites überprüft werden können. Ich habe das Backend zur URL-Prüfung mit Laravel entwickelt.'
+  },
+  {
+    title: 'Geocaching Adventskalender',
+    link: 'https://advent.dalgeo.de',
+    description: 'Geocaching ist die weltweit größte Schatzsuche. Für Rätselfreunde habe ich im Advent einen Adventskalender mit verschiedenen Rätsel erstellt. Diese wurden jeden Tag auf einer eigens mit Laravel entwickelten Website veröffentlicht und die Lösung konnte direkt geprüft werden.'
+  },
+  {
+    title: 'Tim Reiter',
+    link: 'https://tim-reiter.de',
+    description: 'Tim Reiter ist ein Fahrer in der ADAC GT4 Germany. Um sich Interessierten und Sponsoren vorzustellen, erstellte ich eine Website mit Hilfe von WordPress und eigenen Plugins.'
+  },
+  {
+    title: 'Lastenrad Ostfildern',
+    link: 'https://lastenrad-ostfildern.de',
+    description: 'Für das Lastenrad des ADFC Ostfildern erstelle ich mit Hilfe von WordPress, einem Buchungs-Plugins und eigenen Entwicklungen eine Informationswebsite und Buchungssystem.'
   },
   {
     title: 'wunderlich Media',
     link: 'https://wunderlich-media.com',
-    description: 'Website für wunderlich Media.'
+    description: 'Lukas Wunderlich ist Social Media Manager für Rennfahrer. Für ihn erstellte ich eine Website mit Hilfe von WordPress.'
   },
   {
-    title: 'Der Friseur Stuttgart',
-    link: 'https://der-friseur-stuttgart.de',
-    description: 'Website für wunderlich Media.'
+    title: 'Praxis Dr. Medwed',
+    link: 'https://praxis-medwed.de',
+    description: 'Für die Praxis Dr. Medwed in Stuttgart-Ost erstellte ich eine Website mit Hilfe von WordPress.'
   },
   {
     title: 'CVJM Nellingen',
     link: 'https://cvjm-nellingen.de',
-    description: 'Neue Website für den CVJM Nellingen.'
+    description: 'Der CVJM Nellingen benötigte eine neue, leicht zu bearbeitende Website, um Informationen mit Mitgliedern und Interessierten zu teilen. Ich erstellte die Site mit Hilfe von WordPress.'
+  },
+  {
+    title: 'Der Friseur Stuttgart',
+    link: 'https://der-friseur-stuttgart.de',
+    description: 'Für den Friseur Andreas Hillemann durfte ich eine einfache Website erstellen, die gut in Suchmaschinen gefunden wird und eine Verlinkung zum Online-Terminkalender enthält.'
   }
 ]
 
