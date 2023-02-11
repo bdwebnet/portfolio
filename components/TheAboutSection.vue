4136604b7a9aac16a6def1aea3361b59ab1e30b2
bd <51322242+bdwebnet@users.noreply.github.com>
2023-02-11 01:27:43 +0100

    updated about section

--- components/TheAboutSection.vue
+++ components/TheAboutSection.vue
@@ -1,10 +1,36 @@
 <template>
   <div>
     <p>
-      Seit <strong>über 7 Jahren</strong> beschäftige ich mich mit dem Thema Programmierung, insbesondere der Web-Entwicklung.
+      Ich bin ein <strong>begeisterter Web-Entwickler</strong>,
+      der sich bereits seit <strong>mehr als 7 Jahren</strong> mit dem Thema beschäftigt.
+      Diese Leidenschaft begann in der Schule,
+      als ich erstmals mit den Grundlagen von HTML und CSS in Berührung kam.
     </p>
     <p>
-      test
+      Im <strong>Selbstudium</strong> baute ich meine Fähigkeiten
+      im Bereich Web-Entwicklung weiter aus
+      und experimentierte mit verschiedenen Technologien und Frameworks.
+      Bei einem schulischen Praktikum lernte ich zudem <strong>PHP</strong> kennen und begann,
+      mich auch mit dieser Sprache zu beschäftigen.
+    </p>
+    <p>
+      Ich entdeckte auch <strong>WordPress</strong> und nutzte es,
+      um Websites zu erstellen und zu verwalten.
+      Für schulische Projekte erstellte ich mehrere Websites
+      auf Basis dieses Content Management System.
+      Außerdem brachte ich mir die <strong>Serveradministration</strong> bei
+      und <strong>hoste Websites seitdem auf einem eigenen Server</strong>.
+    </p>
+    <p>
+      Nach meinem <strong>Abitur 2021</strong> machte ich mich als Web-Entwickler selbstständig
+      und setzte verschiedene Kundenprojekte mit WordPress um.
+      Ebenfalls lernte ich <strong>Laravel</strong>
+      und <strong>Vue.js</strong>, mit denen ich private Projekte umsetzte.
+    </p>
+    <p>
+      Um mein selbst angeeignetes Wissen zu vertiefen und weitere Fähigkeiten
+      zu erlernen startete ich zum Schuljahr 22/23 eine <strong>Ausbildung zum staatlich geprüften
+        Informatiker</strong> an der Akademie für Datenverarbeitung in Böblingen.
     </p>
   </div>
 </template>
