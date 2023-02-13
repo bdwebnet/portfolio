0a78f9493c99ac62449d6f57da439380192f5c23
bd <51322242+bdwebnet@users.noreply.github.com>
2023-02-13 20:03:20 +0100

    sticky back to home link

--- layouts/subpage.vue
+++ layouts/subpage.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="page-wrapper">
-    <TheHeader />
+    <TheHeader id="header" />
     <slot />
     <TheFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.page-wrapper {
+  position: relative;
+}
+
+#header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  background-color: $secondary;
+}
+</style>
